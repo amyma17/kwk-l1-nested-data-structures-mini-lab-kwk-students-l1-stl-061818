31d07b82ffbb1f58fccc34e31d66558e449b568f
amyma17 <amyma171311@gmail.com>
2018-06-21 16:12:06 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -1,14 +1,39 @@
-# example ingredients array
 ingredients = [
     ["rye", "sourdough", "baguette"],
     ["ham", "salami", "turkey"],
     ["swiss", "munster", "cheddar"],
     ["mayo", "mustard", "tabasco"]
 ]
-
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+  ingredients.each do |sustenance|
+    sustenance.each do |food|
+      if food == order[0]
+        puts "true"
+      elsif food == order[1]
+        puts "true"
+      elsif food == order[2]
+        puts "true "
+      end
+    end 
+  end 
+  # puts order
+# puts ingredients[3]
+# puts ingredients[2][2]
+# puts ingredients[0][1]
+# if order == ingredients[0]
+#   puts "true"
+# elsif order == ingredients[1]
+#   puts "true"
+# elsif order == ingredients[2]
+#   puts "true"
+# elsif order == ingredients[3]
+#   puts "true"
+# else 
+#   puts "false"
+# end
 end
+sandwich_request(ingredients, ["swiss","salami","baguette"] )
+
 
 # example city_info hash
 cities = {
